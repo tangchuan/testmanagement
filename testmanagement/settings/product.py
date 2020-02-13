cfdbdd33a4631b555b00e1de550a3b4302d9cf56
tangchuan <317403442@qq.com>
2020-02-13 11:31:57 +0800

    修改mysql host

--- testmanagement/settings/product.py
+++ testmanagement/settings/product.py
@@ -12,7 +12,7 @@ DATABASES = {
         'NAME': 'testmanagement',    #你的数据库名称
         'USER': 'root',   #你的数据库用户名
         'PASSWORD': '123456', #你的数据库密码
-        'HOST': '', #你的数据库主机，留空默认为localhost
+        'HOST': '192.168.1.8', #你的数据库主机，留空默认为localhost
         'PORT': '3306', #你的数据库端口
     }
 }
